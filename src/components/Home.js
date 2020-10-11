7c800f63b631334711dc47dfc0e7b75136b7a2b6
kumari.nivedita <kumari.nivedita@ril.com>
2020-10-11 16:54:57 +0530

    adding a button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import Button from '@material-ui/core/Button';
+import {useHistory, withRouter} from 'react-router-dom'
 
 function Home() {
     const appState = useSelector(state => state.appState)
     const dispatch = useDispatch()
+    const history = useHistory()
+
+    const onHomeButton = () => {
+        history.push('/login')
+    }
 
     return (
        <React.Fragment>
             Home
+            <Button variant="contained" color="primary" onClick={onHomeButton}> 
+                Primary
+            </Button>
        </React.Fragment>   
     );
   }
